c946f7c6bf2655f39e47532f50c9f395e85786c0
Sonam Lakhanpal <sonamlakhanpal@gmail.com>
2025-05-22 14:50:51 +0530

    changes suggested by git hub co pilot agent 22-05-2025

--- src/test/java/firstTest.java
+++ src/test/java/firstTest.java
@@ -3,45 +3,47 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class firstTest {
    public static WebDriver driver;
 
-
     @Test
-   public void openGoogle() {
-
+    public void openGoogle() {
         /*// instead of system set property for webdriver.chrome.driver and set compatible chromedriver drivers below line use this
         //System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
-
 
         driver = new ChromeDriver();
 
         driver.manage().window().maximize();
 
         driver.get("https://google.com");*/
-
-
     }
 
-
     @Test
-    public void openSC(){
-
+    public void openSC() {
+        // Setup ChromeDriver using WebDriverManager for automatic driver management
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-
         driver.manage().window().maximize();
-
-        driver.get("https://www.sc.com/in/bank-with-us/online-banking-login/");
-        driver.findElement(By.xpath("//button[@title='Login']")).click();
-        WebElement OnlineBanking = driver.findElement(By.xpath("//a[@title='Online Banking Login']"));
-        if(OnlineBanking.isDisplayed()){
-            OnlineBanking.click();
+        try {
+            driver.get("https://www.sc.com/in/bank-with-us/online-banking-login/");
+            // Use WebDriverWait for synchronization
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@title='Login']"))).click();
+            WebElement onlineBanking = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@title='Online Banking Login']")));
+            if (onlineBanking.isDisplayed()) {
+                onlineBanking.click();
+            }
+            // Optionally, wait for the next page or element to ensure navigation
+            // wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@title='Continue to Login']")));
+        } finally {
+            // Ensure the driver quits even if the test fails
+            driver.quit();
         }
-        //driver.findElement(By.xpath("//a[@title='Continue to Login']")).click();
-
-
-
     }
 }
